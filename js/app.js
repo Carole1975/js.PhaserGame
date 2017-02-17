1f1bbc16d1488d804ea1b7615a795c469bbf55ff
raphael <moudens.raphael@orange.fr>
2017-02-17 16:54:12 +0100

    ajout dossier de base

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var game = new Phaser.Game( 800, 500, Phaser.AUTO, 'jeu', { preload: preload, create: create, update: update });
+
+function preload(){
+
+}
+function create(){
+
+}
+function update(){
+	
+}
