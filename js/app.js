6a893c0e6725d660cc172396c76f8b1a7ab929eb
kevin <kevinpuyol@live.fr>
2017-02-20 15:06:24 +0100

    ajout_sprite

--- js/app.js
+++ js/app.js
@@ -3,54 +3,59 @@ var cursors, player, background, facing = 'left', music;
 
 
 function preload() {
-    game.load.image("background", "assets/stage.jpg");
-    game.load.spritesheet("man", "assets/woman.png", 95, 158);
-    // game.load.spritesheet("dude", "images/dude.png", 32, 48);
+    game.load.image("background", "assets/cuisine.png");
+    game.load.spritesheet("woman", "assets/walkingwomangg_720.png", 100, 135);
+    game.load.spritesheet("dude", "assets/mansmall_720.png", 100, 200);
 }
-
-
+var player;
+var png;
 function create() {
-    music = game.add.audio('micka');
-    music.play();
+    //music = game.add.audio('micka');
+    //music.play();
     background = game.add.sprite(0, 0, "background");
-    player = game.add.sprite(70, 370, "dude");
-    player.anchor.setTo(0.5);
+    player = game.add.sprite(70, 370, "woman");
+    png = game.add.sprite(70, 400, "dude");
+    //player.anchor.setTo(0.5);
     // pour enlever moon walk, comment what's down below //
-    player.scale.setTo(-1, 1)
+    //player.scale.setTo(-1, 1)
 
-    Onekey.michael.onDown.add(playFx, this);
+    //Onekey.michael.onDown.add(playFx, this);
 
-    player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 12, true, true);
+    player.animations.add('left', [7, 9, 10, 11, 12, 13,], 20, true, true);
     player.animations.add('turn', [6], 24, true, true);
-    player.animations.add('right', [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23], 12, true, true);
+    player.animations.add('right', [0, 1, 2, 3, 4, 5], 20, true, true);
 
     game.physics.arcade.enable(player)
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
 
     keys = game.input.keyboard.createCursorKeys();
 
 }
 function update() {
+
     player.body.velocity.x = 0;
     player.body.gravity.y = 0;
 
     if (keys.left.isDown) {
         player.body.velocity.x = -200;
+        player.animations.play('left');
 
 
-        if (facing != 'left');
-        {
-            player.animations.play('left')
-            facing = 'left';
-        }
     }
     else if (keys.right.isDown) {
         player.body.velocity.x = 200;
+        player.animations.play('right');
 
-        if (facing != 'right') {
-            player.animations.play('right');
-            facing = 'right';
-        }
+
+    }
+
+    else
+    {
+        player.animations.stop();
+
+        player.frame = 5;
     }
 }
 
